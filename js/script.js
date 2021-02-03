a4eea30bdf2378d5a724f1d0602c17d18bc7ec3d
Usama Nadeem <usama.nadeem77@gmail.com>
2021-02-03 22:26:22 +0500

    Form linked to netlify, facebook code added

--- js/script.js
+++ js/script.js
@@ -16,42 +16,10 @@ $( document ).ready(function() {
 	    readURL(this);
 	});
 
-	$(document).on("submit", "#reg-form",function(e) {
-
-    	e.preventDefault(); 
-
-    	// var form = $(this);
-	    // var url = form.attr('action');
-	    
-	    // $.ajax({
-	    //        type: "POST",
-	    //        url: url,
-	    //        data: form.serialize(), // serializes the form's elements.
-	    //        success: function(data)
-	    //        {
-	    //            alert(data); // show response from the php script.
-	    //        }
-	    //      });
-	    // });
-
-	    $("#reg-form-div").removeClass("d-block").addClass("d-none");
-	    $("#btn-submit").removeClass("d-block").addClass("d-none");
-
-	    $("#thank-you").removeClass("d-none").addClass("d-block");
-	    $("#buy-tickets").removeClass("d-none").addClass("d-block");
-
-	});
-
 	$('#registerModal').on('hidden.bs.modal', function (e) {
 	  	
-	  	$("#thank-you").removeClass("d-block").addClass("d-none");
-	    $("#buy-tickets").removeClass("d-block").addClass("d-none");
-
-	    $("#reg-form-div").removeClass("d-none").addClass("d-block");
-	    $("#btn-submit").removeClass("d-none").addClass("d-block");
-
-	    $("#reg-form").trigger("reset");
+	  	$("#reg-form").trigger("reset");
 	    $("#img-preview").attr("src", "images/avatar.png");
-	})
+	});
 
 });
